05394aa101d5add9ecdb4de3c3b7b3b5202c3c5d
IHamzaI <hamzaaqib@hotmail.com>
2015-11-02 09:53:51 -0500

    Update Archon.java

--- Archon.java
+++ Archon.java
@@ -11,7 +11,7 @@ import arena.Bullet;
 public class Archon extends Bot {
 	
 	String name;
-	
+	private String[] killMessages = {"Get rekt m8", "Gr8 b8 m8!", "eeeeezzzz", "Psyche!", "Eureka!", "Are you trying?", ""};
 	Image up, down, right, left, current;
 	
 	/**
@@ -20,12 +20,15 @@ public class Archon extends Bot {
 	 * bulletleft is cooldown for shooting left
 	 * bulletright is cooldown for shooting right
 	 */
+	private String nextMessage = null;
 	private int bulletdown, bulletup, bulletleft, bulletright;
+	private int msgCounter = 0;
 	//private Random rand;
 	/**
 	 * Current move
 	 */
 	private int move = BattleBotArena.UP;
+	private int shootdir;
 	private int dodgethis;
 	/**
 	 * My last location - used for detecting when I am stuck
@@ -53,21 +56,132 @@ public class Archon extends Bot {
 
 	@Override
 	public int getMove(BotInfo me, boolean shotOK, BotInfo[] liveBots, BotInfo[] deadBots, Bullet[] bullets) {
-		//count down the directional shot cooldowns
-		
-		if (bulletdown >0){
-			bulletdown --;
-		}
-		if (bulletright >0){
-			bulletright --;
-		}
-		if (bulletleft >0){
-			bulletleft --;
-		}
-		if (bulletup >0){
-			bulletup --;
-		}
 		//bullet dodging
+		//dodgescript(bullets, me);
+		//we are going to have another part that overrides the dodge mechanics if bot is stuck.
+		if (shotOK&&dodgemode==0){//only if not dodging
+			shootdir = move;
+			shootscript(liveBots, me, shotOK);
+			return shootdir;
+		}
+		else {
+			return move;
+		}
+	}
+
+	@Override
+	public void draw(Graphics g, int x, int y) {
+		// TODO Auto-generated method stub
+		g.drawImage(current, x, y, RADIUS*2, RADIUS*2, null);
+	}
+	
+	@Override
+	public String getName() {
+		name = "Archon";
+		return name;
+	}
+
+	@Override
+	public String getTeamName() {
+		// TODO Auto-generated method stub
+		return "Archon FTW!";
+	}
+
+	@Override
+	public String outgoingMessage() {
+		String msg = nextMessage;
+		nextMessage = null;
+		return msg;
+	}
+
+	@Override
+	public void incomingMessage(int botNum, String msg) {
+		if (botNum == BattleBotArena.SYSTEM_MSG && msg.matches(".*destroyed by "+getName()+".*"))
+		{
+			int msgNum = (int)(Math.random()*killMessages.length);
+			nextMessage = killMessages[msgNum];
+			msgCounter = (int)(Math.random()*30 + 30);
+		}
+
+
+	}
+
+	@Override
+	public String[] imageNames() {
+		String images[] = {"Archon_UP.gif","Archon_DOWN.png","Archon_LEFT.png","Archon_RIGHT.png"};
+		return images;
+	}
+
+	@Override
+	public void loadedImages(Image[] images) {
+		// TODO Auto-generated method stub
+		if (images != null)
+		{
+			current = up = images[0];
+			down = images[1];
+			left = images[2];
+			right = images[3];
+		}
+	}
+	
+	// not functioning
+	private void dodgeMechanics (Bullet[] bullets,BotInfo me) {
+		
+		int [] move = {1,2,3,4};
+		
+		double maxCheck = RADIUS*4;
+		
+		// if danger booleans are true call a movement method to move out of the way of the bullets
+				
+	}	
+	// not functioning
+	private boolean DangerX(Bullet [] bullets,BotInfo me) {
+		boolean DangerXCor = false;
+		for (int x =0; x<bullets.length;x++) {
+			
+			// Check for dangerous bullets in the x direction by comparing it to the current position of the bot
+			
+		}
+		return DangerXCor;		
+	}
+	//not functioning
+	private boolean DangerY (Bullet [] bullets,BotInfo me) {
+		boolean DangerYCor = false;
+	for (int x =0; x<bullets.length;x++) {
+			
+		// Check for dangerous bullets in the y direction by comparing it to the current position of the bot			
+			
+		}
+	return DangerYCor;
+	}
+	// not functioning
+	private void stuckCheck (BotInfo me,BotInfo deadBots) {
+		
+		double x = me.getX();
+		double y = me.getY();
+		
+		if (y >= BattleBotArena.BOTTOM_EDGE && move == 0) {
+			move = BattleBotArena.UP;			
+		}
+		if (y<= BattleBotArena.TOP_EDGE && move == 0) {
+			move = BattleBotArena.UP;
+		}
+		if (x >= BattleBotArena.RIGHT_EDGE && move == 0) {
+			move = BattleBotArena.UP;
+		}
+		if (x <= BattleBotArena.LEFT_EDGE && move == 0) {
+			move = BattleBotArena.UP;
+		}
+		
+		if (x <= deadBots.getX() && move ==0) {
+			move = BattleBotArena.DOWN;
+		}
+		if (y <= deadBots.getY() && move == 0) {
+			move = BattleBotArena.RIGHT;
+		}
+		
+	}
+	private void dodgescript(Bullet[] bullets, BotInfo me){
 		if (dodgemode == 1) {
 			if(!(bullets[dodgethis].getX()+RADIUS*2 > me.getX() && bullets[dodgethis].getX()-RADIUS*2 < me.getX() 
 					&& bullets[dodgethis].getYSpeed()*(me.getY()-bullets[dodgethis].getY())>0)){
@@ -119,83 +233,48 @@ public class Archon extends Bot {
 				}
 			}
 		}
-		//we are going to have another part that overrides the dodge mechanics if bot is stuck.
-		if (shotOK&&dodgemode==0){//only if not dodging
+	}
+	
+	private void shootscript(BotInfo[] liveBots, BotInfo me, boolean shotOK){
+		if (bulletdown >0){
+			bulletdown --;
+		}
+		if (bulletright >0){
+			bulletright --;
+		}
+		if (bulletleft >0){
+			bulletleft --;
+		}
+		if (bulletup >0){
+			bulletup --;
+		}
 			for(int i = 0; i < liveBots.length; i ++){
 				//this checks if something has same value
 				if (liveBots[i].getX() + 3 > me.getX() && liveBots[i].getX() - RADIUS < me.getX()){
 					if (liveBots[i].getY()-me.getY() > 0 && bulletdown == 0){
 						bulletdown = 10;
-						return BattleBotArena.FIREDOWN;
+						shootdir = BattleBotArena.FIREDOWN;
+						return;
 					}
 					else if (bulletup == 0) {
 						bulletup = 10;
-						return BattleBotArena.FIREUP;
+						shootdir = BattleBotArena.FIREUP;
+						return;
 					}
 				}
 				//
 				else if (liveBots[i].getY() + 3 > me.getY() && liveBots[i].getY() - RADIUS < me.getY()){
 					if (liveBots[i].getX()-me.getX() > 0 && bulletright == 0){
 						bulletright = 10;
-						return BattleBotArena.FIRERIGHT;
+						shootdir = BattleBotArena.FIRERIGHT;
+						return;
 					}
 					else if (bulletleft == 0) {
 						bulletleft = 10;
-						return BattleBotArena.FIRELEFT;
+						shootdir = BattleBotArena.FIRELEFT;
+						return;
 					}
 				}
 			}
-		}
-		
-		return move;
+			
 	}
-
-	@Override
-	public void draw(Graphics g, int x, int y) {
-		// TODO Auto-generated method stub
-		g.drawImage(current, x, y, RADIUS*2, RADIUS*2, null);
-	}
-
-	@Override
-	public String getName() {
-		name = "Archon";
-		return name;
-	}
-
-	@Override
-	public String getTeamName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String outgoingMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void incomingMessage(int botNum, String msg) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public String[] imageNames() {
-		String images[] = {"starfish4.png"};
-		return images;
-	}
-
-	@Override
-	public void loadedImages(Image[] images) {
-		// TODO Auto-generated method stub
-		if (images != null)
-		{
-			current = up = images[0];
-			down = images[0];
-			left = images[0];
-			right = images[0];
-		}
-	}
-
-}
